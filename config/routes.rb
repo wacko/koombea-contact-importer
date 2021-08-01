d21de61d4e908fb3f3b7d1ea25c2cf9309ab95ae
Joaquín Vicente <joakin@gmail.com>
2021-08-01 05:21:50 -0300

    Show contacts

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,12 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   devise_for :users
+
   resources :uploads, only: [:index, :new, :create, :destroy] do
     post :import, on: :member
   end
+  resources :contacts, only: [:index]
+
+  get '/uploads', to: 'uploads#index', as: :user_root
   root to: 'pages#index'
 end
