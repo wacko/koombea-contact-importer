46f122567d56537bb876ab0ce94cc0ead6824bd8
Joaquín Vicente <joakin@gmail.com>
2021-07-31 22:54:34 -0300

    Add log in / sign up

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   #   :recoverable, :validatable, :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :rememberable
+
+  validates_confirmation_of :password, only: :create
 end
---
+++ spec/system/user_sessions_spec.rb
@@ -0,0 +1,40 @@
+require 'rails_helper'
+
+RSpec.describe 'User sessions', type: :system do
+  it 'sucessful sign up' do
+    visit new_user_registration_url
+
+    fill_in 'Email', with: 'new_user@example.com'
+    fill_in 'Password', with: 'password'
+    fill_in 'Password confirmation', with: 'password'
+    click_button 'Sign up'
+
+    expect(User.count).to eq(1)
+    expect(current_path).to eq(root_path)
+    expect(page).to have_text('Sign out')
+  end
+
+  it 'login with an existing user' do
+    User.create(email: 'login_user@example.com', password: 'password')
+
+    visit new_user_session_url
+
+    fill_in 'Email', with: 'login_user@example.com'
+    fill_in 'Password', with: 'password'
+    click_button 'Log in'
+
+    expect(current_path).to eq(root_path)
+    expect(page).to have_text('Sign out')
+  end
+
+  it 'reject login with invalid credentials' do
+    visit new_user_session_url
+
+    fill_in 'Email', with: 'other@example.com'
+    fill_in 'Password', with: 'password'
+    click_button 'Log in'
+
+    expect(current_path).to eq(new_user_session_path)
+    expect(page).to have_text('Log in')
+  end
+end
