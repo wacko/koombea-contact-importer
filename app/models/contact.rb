9a0465392da4eeb9b1760f755f0b424476643e50
Joaquín Vicente <joakin@gmail.com>
2021-08-01 05:37:04 -0300

    Add email validation

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -2,5 +2,5 @@ class Contact < ApplicationRecord
   belongs_to :user
 
   validates :name, format: { with: /\A[a-zA-Z -]+\z/, message: 'only allows letters' }
-  validates :email, uniqueness: { scope: :user_id }
+  validates :email, email: true, uniqueness: { scope: :user_id }
 end
