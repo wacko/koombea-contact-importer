b4cc69845d1162e3267e235d1f99c4d1c233549f
Joaquín Vicente <joakin@gmail.com>
2021-08-01 06:05:41 -0300

    More validations

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -4,4 +4,5 @@ class Contact < ApplicationRecord
   validates :name, format: { with: /\A[a-zA-Z -]+\z/, message: 'only allows letters' }
   validates :email, email: true, uniqueness: { scope: :user_id }
   validates :phone, phone: true
+  validates :address, presence: true
 end
