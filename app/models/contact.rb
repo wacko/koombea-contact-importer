5a38e6a7a757fb7273925de8307eec113676f072
Joaquín Vicente <joakin@gmail.com>
2021-08-01 05:25:03 -0300

    Don't allow duplicated emails

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -2,4 +2,5 @@ class Contact < ApplicationRecord
   belongs_to :user
 
   validates :name, format: { with: /\A[a-zA-Z -]+\z/, message: 'only allows letters' }
+  validates :email, uniqueness: { scope: :user_id }
 end
